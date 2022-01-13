8f06a1c8e675e159f6db20ab7b27c806c40e0c4e
Dmitriy Berezikov <dberezikov@msk.bcs.ru>
2022-01-13 17:10:11 +0000

    Done homework #6

---
+++ terraform/variables.tf
@@ -0,0 +1,37 @@
+variable cloud_id {
+  description = "Cloud"
+}
+variable folder_id {
+  description = "Folder"
+}
+variable zone {
+  description = "Zone"
+  # Значение по умолчанию
+  default = "ru-central1-a"
+}
+variable region {
+  description = "Region"
+  # Значение по умолчанию
+  default = "ru-central1"
+}
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+variable private_key_path {
+  # Описание переменной
+  description = "Path to the private key used for ssh access"
+}
+variable image_id {
+  description = "Disk image"
+}
+variable subnet_id {
+  description = "Subnet"
+}
+variable service_account_key_file {
+  description = "key .json"
+}
+variable instances_count {
+  description = "Count of instances"
+  default     = 1
+}
