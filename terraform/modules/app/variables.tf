2a3e43eac61a55f14f19de8a6f45a6320f84468e
Dmitriy Berezikov <dberezikov@msk.bcs.ru>
2022-01-19 14:52:44 +0000

    Done homework #7

--- terraform/modules/app/variables.tf
+++ terraform/modules/app/variables.tf
@@ -3,8 +3,10 @@ variable app_instance_name {
   default     = "reddit-app"
 }
 variable public_key_path {
-  # Описание переменной
   description = "Path to the public key used for ssh access"
+}
+variable private_key_path {
+  description = "Path to the private key used for ssh access"
 }
 variable subnet_id {
   description = "Subnets for modules"
