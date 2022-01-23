662a81fa86fc4cf16d81e2dfd5cc7d6ea9bb57eb
Dmitriy Berezikov <dberezikov@msk.bcs.ru>
2022-01-23 09:08:33 +0000

    correct terraform/prod file

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -15,6 +15,7 @@ module "app" {
 module "db" {
   source            = "../modules/db"
   public_key_path   = var.public_key_path
+  private_key_path  = var.private_key_path
   db_disk_image     = var.db_disk_image
   subnet_id         = var.subnet_id
   db_instance_name  = var.db_instance_name
