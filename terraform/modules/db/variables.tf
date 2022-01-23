72b27761356e024b9171b5c16fd6a5df4e88b212
Dmitriy Berezikov <dberezikov@msk.bcs.ru>
2022-01-23 08:37:46 +0000

    Done homework #8

--- terraform/modules/db/variables.tf
+++ terraform/modules/db/variables.tf
@@ -5,6 +5,9 @@ variable db_instance_name {
 variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
+variable private_key_path {
+  description = "Path to the private key used for ssh access"
+}
 variable subnet_id {
   description = "Subnets for modules"
 }
